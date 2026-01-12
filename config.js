b2c87987502678262693aabeded7e42f1f51941c
VIN-WILSON207 <abilavinwilson@gmail.com>
2026-01-12 21:55:37 +0100

    add config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// Supabase Configuration
+
+const CONFIG = {
+  SUPABASE_URL: 'https://myvopjdovcphzpxpxriw.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_Fv7DOp0Ebdf3Zc1SoY74wg_T24DTZMu'
+};
+
+// Do not modify below this line
+if (typeof window !== 'undefined') {
+  window.CONFIG = CONFIG;
+}
